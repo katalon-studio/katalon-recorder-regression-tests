cb3448f570194ab203bf3e37b68d261103e5dcb5
tientruong209 <tien.truong@katalon.com>
2021-08-13 16:57:01 +0700

    experiment with window object

--- jobs/data.job.js
+++ jobs/data.job.js
@@ -27,7 +27,9 @@ async function removeExistingData(extensionPage) {
 
 
 async function loadTestSuiteToExtension(extensionPage, data) {
-    await new Promise(resolve => setTimeout(resolve, 5000));
+    const typeOf = await extensionPage.evaluate(() => typeof window.readSuiteFromString);
+    throw new Error(JSON.stringify(typeOf));
+
     return extensionPage.evaluate(async function(data) {
         window.open("www.google.com");
         readSuiteFromString(data);
