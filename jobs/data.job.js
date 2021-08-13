127bdadca3a782a52a5692ebba63d378f5cc6858
tientruong209 <tien.truong@katalon.com>
2021-08-13 22:44:03 +0700

    experiment with window object

--- jobs/data.job.js
+++ jobs/data.job.js
@@ -27,6 +27,7 @@ async function removeExistingData(extensionPage) {
 
 
 async function loadTestSuiteToExtension(extensionPage, data) {
+    await new Promise(resolve => setTimeout(resolve, 2000));
 
     /*const module = await extensionPage.evaluate(async function(){
         let module = await import("./js/UI/view/records-grid/record-utils.js");
