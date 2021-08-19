4573552c983e201a53063bdb226204e5011c3bb1
tientruong209 <tien.truong@katalon.com>
2021-08-19 17:29:54 +0700

    optimized end to end test

--- tests/testcase.test.js
+++ tests/testcase.test.js
@@ -7,19 +7,19 @@ describe("Run test case", () => {
         let result = await TestCaseService.runTestcase(page);
         await expect(result).toBe(true);
         await browser.close();
-    }, 100000);
+    }, 200000);
 
     it.concurrent("Users can execute parts of a test case with Play From Here from a test case's context menu", async() => {
         const {browser, page} = await ExtensionService.getPageAndData("sample/data.html");
         let result = await TestCaseService.runFromSpecifiedCommandOfTestcase(page);
         await expect(result).toBe(true);
         await browser.close();
-    }, 100000);
+    }, 200000);
 
     it.concurrent("Users can execute a command with Play This Command from a test case’s context menu.", async() => {
         const {browser, page} = await ExtensionService.getPageAndData("sample/data.html");
         let result = await TestCaseService.runCommandOfTestCase(page);
         await expect(result).toBe(true);
         await browser.close();
-    }, 10000);
+    }, 200000);
 });
