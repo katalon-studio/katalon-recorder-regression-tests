3b88788e2fcbfe026c8ee84825e7d1e88908ede4
tien.truong <tien.truong@katalon.com>
2021-10-22 16:51:42 +0700

    new test for 5.6.0 version

--- tests/testsuite.test.js
+++ tests/testsuite.test.js
@@ -4,12 +4,13 @@ const TestSuiteService = require('../services/testsuite.service');
 describe("Run test suite", () => {
     it.concurrent("Users can execute a test suite with Play Suite", async() => {
         const extension = await ExtensionService.getPageAndData("sample/data.html");
+        throw extension.page;
         const browser = extension.browser;
         const extensionPage= extension.page;
         const result = await TestSuiteService.runTestSuite(extensionPage);
         await expect(result).toMatchObject({ pass: 14, fail: 2 });
         await browser.close();
-    }, 300000);
+    }, 200000);
 
     it.concurrent("Users can execute all suites with Play All", async() => {
         const extension = await ExtensionService.getPageAndData("sample/data.html");
@@ -18,7 +19,7 @@ describe("Run test suite", () => {
         const result = await TestSuiteService.runAllTestSuites(extensionPage);
         await expect(result).toMatchObject({ pass: 14, fail: 2 });
         await browser.close();
-    }, 300000)
+    }, 200000)
 
     it.concurrent("Run a test suite from specified test case", async() => {
         const extension = await ExtensionService.getPageAndData("sample/data.html");
@@ -27,5 +28,5 @@ describe("Run test suite", () => {
         const result = await TestSuiteService.runFromSpecifiedTestcase(extensionPage);
         expect(result).toMatchObject({ pass: 13, fail: 2 });
         await browser.close();
-    }, 300000)
+    }, 200000)
 });
